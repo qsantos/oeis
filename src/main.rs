8d2d8cf999dd0f0751bd7cf69c21b74b36e072c0
Quentin Santos <qsantos@qsantos.fr>
2023-02-12 19:16:09 +0100

    Parallelize with Rayon

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::io::prelude::*;
 use std::io::stdout;
 
+use rayon::prelude::IntoParallelIterator;
+use rayon::prelude::ParallelIterator;
+
 fn is_counter_example(n: u128) -> bool {
     for p in [7, 11, 13, 17, 31, 37, 41, 43, 59, 61, 67, 73, 79] {
         if n % p == 0 {
@@ -26,14 +29,23 @@ fn main() {
     assert!(!is_counter_example(5));
     assert!(is_counter_example(solution));
 
-    let mut n = 1;
-    while !is_counter_example(n) {
-        if n % 1048576 == 1 {
-            print!("\r{:6.2} %", (n as f32) / (solution as f32) * 100.);
-            stdout().flush().expect("Could not flush");
+    let mut chunk_start = 0;
+    let chunk_size = 1_000_000;
+    let n = loop {
+        let chunk_end = chunk_start + chunk_size;
+        if let Some(n) = (chunk_start..chunk_end)
+            .into_par_iter()
+            .find_first(|&n| is_counter_example(1 + 4 * n))
+        {
+            break 1 + 4 * n;
         }
-        n += 4;
-    }
+        print!(
+            "\r{:6.2} %",
+            ((1 + 4 * chunk_end) as f32) / (solution as f32) * 100.
+        );
+        stdout().flush().expect("Could not flush");
+        chunk_start = chunk_end;
+    };
     println!();
     println!("{n} is a counter example");
     assert!(is_counter_example(n));
